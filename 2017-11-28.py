d0191bac9c5e3036b484e5aefeecd1b702e359fe
githubxiaoqin <1324641287@qq.com>
2017-11-28 22:44:35 +0800

    2017-11-28

---
+++ 2017-11-28.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+#1、加法
+a = 100
+b = 10
+c = a + b
+
+# print ("Line 1 - value of c is",c)
+print( "a + b =", c)
+
+
+#2、减法
+c = a - b
+# print("line 2 - value of c is",c)
+print("a - b =",c)
+
+#3、乘法
+c = a * b
+print("a * b =",c)
+
+#4、除法
+c = a / b
+print("a / b =",c)
+
+#5、取余
+c = a % b
+print("a % b =",c)
+
+#6、幂
+c = a ** b
+print("a ** b =",c)
+
+
+#7、取整除
+c = a // b
+print("a // b =",c)
